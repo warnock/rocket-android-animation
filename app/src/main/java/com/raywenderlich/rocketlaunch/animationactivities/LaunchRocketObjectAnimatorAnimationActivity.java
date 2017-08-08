c8ab90a6eccc6e50e64186a095318f29834983a8
Megan Warnock <megan@dealerpeak.com>
2017-08-08 14:32:42 -0700

    add object animator - launch a rocket (objectAnimator)

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.java
@@ -1,8 +1,15 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.ObjectAnimator;
+
 public class LaunchRocketObjectAnimatorAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+    ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mRocket, "translationY",
+            0, -mScreenHeight);
+
+    objectAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+    objectAnimator.start();
 
   }
 }
