b08ac714a91e3a3060ada9d337b2263875bc7c10
Megan Warnock <megan@dealerpeak.com>
2017-08-08 14:06:40 -0700

    set first animation - launch a rocket

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketValueAnimatorAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketValueAnimatorAnimationActivity.java
@@ -1,8 +1,24 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.ValueAnimator;
+import android.view.animation.LinearInterpolator;
+
 public class LaunchRocketValueAnimatorAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+      ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, -mScreenHeight);
 
+      valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+          @Override
+          public void onAnimationUpdate(ValueAnimator animation) {
+              float value = (float) animation.getAnimatedValue();
+              mRocket.setTranslationY(value);
+          }
+      });
+
+      valueAnimator.setInterpolator(new LinearInterpolator());
+      valueAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+
+      valueAnimator.start();
   }
 }
