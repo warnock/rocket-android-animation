a2c9e9ab9ff46c5596dbf7808a901133a4ca52bb
Megan Warnock <megan@dealerpeak.com>
2017-08-08 15:23:16 -0700

    animate both rocket and dog

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/FlyWithDogeAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/FlyWithDogeAnimationActivity.java
@@ -1,8 +1,35 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+
 public class FlyWithDogeAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+
+    ValueAnimator positionAnimator = ValueAnimator.ofFloat(0, -mScreenHeight);
+    positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setTranslationY(value);
+        mDoge.setTranslationY(value);
+      }
+    });
+
+    ValueAnimator rotationAnimator = ValueAnimator.ofFloat(0, 360);
+    rotationAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mDoge.setRotation(value);
+      }
+    });
+
+    AnimatorSet animatorSet = new AnimatorSet();
+    animatorSet.play(positionAnimator).with(rotationAnimator);
+    animatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
+    animatorSet.start();
 
   }
 }
