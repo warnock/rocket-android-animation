92010aabbec6785f68be3af0d0c6a8c88223d15f
Megan Warnock <megan@dealerpeak.com>
2017-08-08 15:09:13 -0700

    add launch and spin animation - AnimatorSet

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/ColorAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/ColorAnimationActivity.java
@@ -1,9 +1,23 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.support.v4.content.ContextCompat;
+
+import com.raywenderlich.rocketlaunch.R;
+
 public class ColorAnimationActivity extends BaseAnimationActivity {
 
   @Override
-  protected void onStartAnimation() {
+    protected void onStartAnimation() {
+      ObjectAnimator objectAnimator = ObjectAnimator.ofObject(mFrameLayout, "backgroundColor", new ArgbEvaluator(), ContextCompat.getColor(this, R.color.background_from), ContextCompat.getColor(this, R.color.background_to));
 
+    objectAnimator.setRepeatCount(1);
+    objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+    objectAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+    objectAnimator.start();
+    }
   }
-}
+
--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchAndSpinAnimatorSetAnimatorActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchAndSpinAnimatorSetAnimatorActivity.java
@@ -1,8 +1,28 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+
 public class LaunchAndSpinAnimatorSetAnimatorActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+    ValueAnimator positionAnimator = ValueAnimator.ofFloat(0, -mScreenHeight);
 
+    positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setTranslationY(value);
+      }
+    });
+
+    ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(mRocket, "rotation", 0, 180f);
+
+    AnimatorSet animatorSet = new AnimatorSet();
+    animatorSet.play(positionAnimator).with(rotationAnimator);
+
+    animatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
+    animatorSet.start();
   }
 }
