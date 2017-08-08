afb962cd11f19a24962078343bf20d765294eb40
Megan Warnock <megan@dealerpeak.com>
2017-08-08 14:18:31 -0700

    add spin animation - spin a rocket

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketValueAnimatorAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchRocketValueAnimatorAnimationActivity.java
@@ -1,14 +1,16 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
 import android.animation.ValueAnimator;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
 
 public class LaunchRocketValueAnimatorAnimationActivity extends BaseAnimationActivity {
+
   @Override
   protected void onStartAnimation() {
       ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, -mScreenHeight);
-
       valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
           @Override
           public void onAnimationUpdate(ValueAnimator animation) {
               float value = (float) animation.getAnimatedValue();
@@ -20,5 +22,5 @@ public class LaunchRocketValueAnimatorAnimationActivity extends BaseAnimationAct
       valueAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
 
       valueAnimator.start();
-  }
+      }
 }
--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/RotateRocketAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/RotateRocketAnimationActivity.java
@@ -1,8 +1,24 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.ValueAnimator;
+import android.view.animation.LinearInterpolator;
+
 public class RotateRocketAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+    ValueAnimator animator = ValueAnimator.ofFloat(0, 360);
+
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setRotation(value);
+      }
+    });
+
+    animator.setInterpolator(new LinearInterpolator());
+    animator.setDuration(DEFAULT_ANIMATION_DURATION);
+    animator.start();
 
   }
 }
