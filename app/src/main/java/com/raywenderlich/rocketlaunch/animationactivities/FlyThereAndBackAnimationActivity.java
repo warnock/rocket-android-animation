4d2801b9478bd3d254575d85da9eb48f74460102
Megan Warnock <megan@dealerpeak.com>
2017-08-08 15:42:34 -0700

    add a revese animation - there and back

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/FlyThereAndBackAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/FlyThereAndBackAnimationActivity.java
@@ -1,8 +1,26 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.ValueAnimator;
+
 public class FlyThereAndBackAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+    ValueAnimator animator = ValueAnimator.ofFloat(0, -mScreenHeight);
 
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setTranslationY(value);
+        mDoge.setTranslationY(value);
+      }
+    });
+
+    animator.setRepeatMode(ValueAnimator.REVERSE);
+
+    animator.setRepeatCount(3);
+
+    animator.setDuration(500L);
+    animator.start();
   }
 }
