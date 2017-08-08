3443ecd3a73ed8831bcdd3392fd8f5c81f8872dc
Megan Warnock <megan@dealerpeak.com>
2017-08-08 15:34:52 -0700

    add animation listeners - animation events

--- app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/WithListenerAnimationActivity.java
+++ app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/WithListenerAnimationActivity.java
@@ -1,8 +1,51 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.widget.Toast;
+
 public class WithListenerAnimationActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+
+    ValueAnimator animator = ValueAnimator.ofFloat(0, -mScreenHeight);
+
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setTranslationY(value);
+        mDoge.setTranslationY(value);
+      }
+    });
+
+    animator.addListener(new Animator.AnimatorListener() {
+      @Override
+      public void onAnimationStart(Animator animation) {
+        Toast.makeText(WithListenerAnimationActivity.this, "Doge took off", Toast.LENGTH_SHORT)
+                .show();
+      }
+
+      @Override
+      public void onAnimationEnd(Animator animation) {
+        Toast.makeText(WithListenerAnimationActivity.this, "Doge is on the moon", Toast.LENGTH_SHORT)
+                .show();
+        finish();
+      }
+
+      @Override
+      public void onAnimationCancel(Animator animation) {
+
+      }
+
+      @Override
+      public void onAnimationRepeat(Animator animation) {
+
+      }
+    });
+
+    animator.setDuration(5000l);
+    animator.start();
 
   }
 }
